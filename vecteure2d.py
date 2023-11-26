90123bf01c85d20aa796438eedf0d560afa86fb9
WAHIBAEZZIRARI <146207125+WAHIBAEZZIRARI@users.noreply.github.com>
2023-11-26 19:45:08 +0100

    Add files via upload

---
+++ vecteure2d.py
@@ -0,0 +1,37 @@
+class vectur2D :
+     def __init__( self,X,Y):
+        self.X = X
+        self.Y = Y
+        vectur2D . nmbre += 1
+     def getX (self):
+         return self. X
+     
+     def setX (self,X):
+         return self.X==X
+     
+     def getY(self):
+         return self.Y
+     
+     def setY(self,Y):
+         return self.Y
+     
+     def getNMBRE (self):
+         return self.NMB
+     
+     def tostringe (self):
+         return (self,X ), (self,Y)
+     
+     def equals(self):
+         return self.equals
+     
+
+
+              
+     
+
+        
+     
+     
+     
+     
+     
